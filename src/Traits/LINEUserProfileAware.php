cdd9d40bb861f129e14f72fd1b080aabcaad766d
koramit <koramit.p@gmail.com>
2025-03-16 20:08:43 +0700

    update relationship

--- src/Traits/LINEUserProfileAware.php
+++ src/Traits/LINEUserProfileAware.php
@@ -2,23 +2,13 @@
 
 namespace Koramit\LaravelLINEBot\Traits;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Koramit\LaravelLINEBot\Models\LINEUserProfile;
 
 trait LINEUserProfileAware
 {
-    public function lineProfiles(): HasMany
+    public function lineProfile(): BelongsTo
     {
-        return $this->hasMany(LINEUserProfile::class);
-    }
-
-    public function activeLineProfile(): HasOne
-    {
-        return $this->hasOne(LINEUserProfile::class)
-            ->ofMany(['updated_at' => 'max'], function (Builder $query) {
-                $query->where('line_user_id', $this->line_user_id);
-            });
+        return $this->belongsTo(LINEUserProfile::class);
     }
 }
