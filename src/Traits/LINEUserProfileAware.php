e52ee702676cf045c870d649d78722ac463c4896
koramit <koramit.p@gmail.com>
2025-03-17 09:53:43 +0700

    change relationship type

--- src/Traits/LINEUserProfileAware.php
+++ src/Traits/LINEUserProfileAware.php
@@ -2,13 +2,13 @@
 
 namespace Koramit\LaravelLINEBot\Traits;
 
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Koramit\LaravelLINEBot\Models\LINEUserProfile;
 
 trait LINEUserProfileAware
 {
-    public function lineProfile(): HasOne
+    public function lineProfiles(): HasMany
     {
-        return $this->HasOne(LINEUserProfile::class);
+        return $this->hasMany(LINEUserProfile::class);
     }
 }
